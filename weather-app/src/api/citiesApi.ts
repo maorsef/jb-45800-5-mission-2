b98f238c99a3244bbbe7bb090a43f62ee2fe5fdd
maor sefti <maorsefti.fs@gmail.com>
2026-04-15 02:43:14 +0300

    final-mission

--- weather-app/src/api/citiesApi.ts
+++
@@ -1,7 +0,0 @@
-export const getCities = async () => {
-  const res = await fetch(
-    "https://data.gov.il/api/3/action/datastore_search?resource_id=8f714b6f-c35c-4b40-a0e7-547b675eee0e"
-  );
-  const data = await res.json();
-  return data.result.records;
-};
